38a19da2402807952697a4e11d48457b6ec55c75
zafar <zafar@DESKTOP-GNACMFJ>
2021-02-19 11:26:29 +0530

    commit

---
+++ IOCProj48Anno-MethodReplacer-Injection/src/main/java/com/nit/replacer/BankLoanIntrestCalculatorReplacer.java
@@ -0,0 +1,22 @@
+package com.nit.replacer;
+
+import java.lang.reflect.Method;
+import org.springframework.beans.factory.support.MethodReplacer;
+import org.springframework.stereotype.Component;
+
+
+@Component("blicReplacer")
+public class BankLoanIntrestCalculatorReplacer implements MethodReplacer {
+
+	@Override
+	public Object reimplement(Object obj, Method method, Object[] args) throws Throwable {
+		System.out.println("BankLoanIntrestCalculatorReplacer.reimplement(-,-,-)(Simple Intrest amount)");
+		float pAmt=(float)args[0];
+		float rate=(float)args[1];
+		float time=(float)args[2];
+		
+		//intrest calcualation(simple intrest calculation)
+		return (pAmt*time*rate)/100.0f;
+	}
+
+}
