aaab656c18f0befbe1962823ef588e704ce0ae50
zafar <zafar@DESKTOP-GNACMFJ>
2021-03-08 21:22:32 +0530

    update

--- IOCProj61-Customer-PropertyEditor/src/main/java/com/nit/beans/BankLoanInterestCalculator.java
+++ IOCProj61-Customer-PropertyEditor/src/main/java/com/nit/beans/BankLoanInterestCalculator.java
@@ -1,18 +1,15 @@
 package com.nit.beans;
 
 public class BankLoanInterestCalculator {
-	private IntrestAmountDetails details;
-
 	
-    
+	  private IntrestAmountDetails details;
+   
 	public void setDetails(IntrestAmountDetails details) {
 		this.details = details;
 	}
 
-
-
 	public float calculateIntrestAmount() {	
-		return (details.getPamt()*details.getPamt()*details.getTime())/100.0f;
+		return (details.getPamt()*details.getRate()*details.getTime())/100.0f;
 		
 	}
 	
--- IOCProj61-Customer-PropertyEditor/src/main/java/com/nit/test/CustomeEditorTest.java
+++ IOCProj61-Customer-PropertyEditor/src/main/java/com/nit/test/CustomeEditorTest.java
@@ -1,7 +1,7 @@
 package com.nit.test;
 
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.nit.beans.BankLoanInterestCalculator;
 
@@ -9,13 +9,15 @@ public class CustomeEditorTest {
 
 	public static void main(String[] args) {
 		
-		DefaultListableBeanFactory factory=new DefaultListableBeanFactory();
-		XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader(factory);
-		reader.loadBeanDefinitions("com/nit/cfgs/applicationContext.xml");
+		/*
+		 * DefaultListableBeanFactory factory=new DefaultListableBeanFactory();
+		 * XmlBeanDefinitionReader reader=new XmlBeanDefinitionReader(factory);
+		 * reader.loadBeanDefinitions("com/nit/cfgs/applicationContext.xml");
+		 */
+		ApplicationContext cxt=new ClassPathXmlApplicationContext("com/nit/cfgs/applicationContext.xml");
+		BankLoanInterestCalculator calculator=cxt.getBean("blic",BankLoanInterestCalculator.class);
 		
-		BankLoanInterestCalculator calculator=factory.getBean("blic",BankLoanInterestCalculator.class);
-		
-		System.out.println("Interest ::  " +calculator.calculateIntrestAmount());
+		System.out.println("Interest Amount ::  " +calculator.calculateIntrestAmount());
 
 	}
 
