73a376ff6bfd7d13c51ab13f4e33100534aeddef
zafar <zafar@DESKTOP-GNACMFJ>
2021-03-24 22:46:12 +0530

    profile with yml

---
+++ IOCProj69-BootLayredApp-Customer-Profiels-yml/src/main/java/com/nit/bo/CustomerBo.java
@@ -0,0 +1,14 @@
+package com.nit.bo;
+
+import lombok.Data;
+@Data
+public class CustomerBo {
+	
+	private String  cname;
+	private String cadd;
+	private float pamt;
+	private float rate;
+	private float time;
+	private float intrAmt;
+		
+}
---
+++ IOCProj69-BootLayredApp-Customer-Profiels-yml/src/main/java/com/nit/service/CustomerMgmtServiceImpl.java
@@ -0,0 +1,47 @@
+package com.nit.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.nit.bo.CustomerBo;
+import com.nit.dao.ICustomerDAO;
+import com.nit.dto.CustomerDTO;
+
+@Service("custService")
+public final class CustomerMgmtServiceImpl implements ICustomerMgmtService {
+	@Autowired
+	private ICustomerDAO dao;
+		
+	
+
+	public CustomerMgmtServiceImpl() {
+		System.out.println("CustomerMgmtServiceImpl-0-param constructor");
+	}
+
+
+
+	@Override
+	public String calculateSimpleIntrestAmount(CustomerDTO dto) throws Exception {
+		//write b.logic to calculate simple interestAmount based on pAmt,rate,time
+		float intrAmt=(dto.getPAmt()*dto.getRate()*dto.getTime())/100.0f;
+		 
+		//prepare CustomerBo object having persistable data
+		CustomerBo bo=new CustomerBo();
+		
+		bo.setCname(dto.getCname());
+		bo.setCadd(dto.getCadd());
+		bo.setPamt(dto.getPAmt());
+		bo.setTime(dto.getTime());
+		bo.setRate(dto.getRate());
+		bo.setIntrAmt(intrAmt);
+		
+		//use DAO
+		int count=dao.insert(bo);
+		
+		//immeridate if or ternary operator <code>?<sucess>:<failuare>
+		
+		return count==0?"Customer Registration Failed":"Customer Registration Succeded :: Details are :: Principle Amount :: " +dto.getPAmt()+" Time :: "+dto.getTime()+" Rate :: "+dto.getRate()+" InerestAmount :: "+intrAmt;
+	}
+
+}
